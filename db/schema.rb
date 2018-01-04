d2c4a42108c7cd555e79a0df838ca798b3103dbd
Brad Carlson <b2carlso@yahoo.com>
2018-01-04 21:39:16 +0000

    deleted dev and schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,17 @@
 
 ActiveRecord::Schema.define(version: 20180104010645) do
 
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.string "age"
+    t.string "trainer_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
+
 end
