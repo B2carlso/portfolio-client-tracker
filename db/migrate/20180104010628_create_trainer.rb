b60ee2a0ca1fb2dfbff39f5cbc41b912deb5b01d
Brad Carlson <b2carlso@yahoo.com>
2018-01-04 01:14:04 +0000

    created tables for trainer and clients

--- db/migrate/20180104010628_create_trainer.rb
+++ db/migrate/20180104010628_create_trainer.rb
@@ -1,4 +1,8 @@
 class CreateTrainer < ActiveRecord::Migration[5.1]
   def change
+    create_table :trainers do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
   end
 end
--- db/migrate/20180104010645_create_client.rb
+++ db/migrate/20180104010645_create_client.rb
@@ -1,4 +1,8 @@
 class CreateClient < ActiveRecord::Migration[5.1]
   def change
+    create_table :clients do |t|
+      t.string :name
+      t.string :age
+      t.string :trainer_id
   end
 end
