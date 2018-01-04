73202258d465a0d93a32d39ea3b033a8dc30447e
Brad Carlson <b2carlso@yahoo.com>
2018-01-04 18:35:14 +0000

    added username to trainer table

--- db/migrate/20180104010628_create_trainer.rb
+++ db/migrate/20180104010628_create_trainer.rb
@@ -2,6 +2,7 @@ class CreateTrainer < ActiveRecord::Migration[5.1]
   def change
     create_table :trainers do |t|
       t.string :name
+      t.string :username
       t.string :email
       t.string :password_digest
   end
