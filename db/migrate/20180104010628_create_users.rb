c7fd9e52e98d023ca95d611d4578b2ca307d8d1f
B2carlso <b2carlso@yahoo.com>
2018-01-04 12:50:42 -0800

    Update and rename 20180104010628_create_trainer.rb to 20180104010628_create_users.rb

--- db/migrate/20180104010628_create_users.rb
+++ db/migrate/20180104010628_create_users.rb
@@ -1,6 +1,6 @@
-class CreateTrainer < ActiveRecord::Migration[5.1]
+class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :trainers do |t|
+    create_table :users do |t|
       t.string :name
       t.string :username
       t.string :email
