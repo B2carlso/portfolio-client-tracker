0adaa5e5c186a9222d37893cc7220a6a46534cf6
Brad Carlson <b2carlso@yahoo.com>
2018-01-04 18:33:46 +0000

    worked on controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+   set :session_secret, "trainer_client_tracker"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:trainer_id]
+    end
+
+    def current_trainer
+      Trainer.find(session[:trainer_id])
+    end
+  end
 end
--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,4 +1,14 @@
 require './config/environment'
 
 class TrainersController < ApplicationController
+
+  get '/signup' do
+   # binding.pry
+    if !logged_in?
+       erb :'trainers/new'
+     else
+        redirect to '/clients'
+      end
+
+  end
 end
