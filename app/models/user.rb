fa785af11128f983860b7fbfa4780bcf7d79268f
Brad Carlson <b2carlso@yahoo.com>
2018-02-14 21:30:22 +0000

    added validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,16 @@ class User < ActiveRecord::Base
 
   has_many :clients
   has_secure_password
+
+  validates_presence_of :username, :email, :password
+
+  def slug
+    self.username.gsub(/\s+/, '-').downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.find do |obj|
+      obj.slug == slug
+    end
+  end
 end
